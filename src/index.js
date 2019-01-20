8b565c7c9a2235350279c33156ec8e01cb06a1a0
letnoxn <letnoxn@163.com>
2019-01-20 21:05:41 +0800

    write login copmponent

--- src/index.js
+++ src/index.js
@@ -1,36 +1,35 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import {createStire,applymiddleware,compose} from 'redux'
+import {createStore,applyMiddleware, compose} from 'redux'
 import thunk from 'redux-thunk'
 import { Provider } from 'react-redux'
 import { BrowserRouter, Route, Link, Redirect, Switch } from 'react-router-dom'
 
-import './index.css';
+// import reducers from './reducer'
 import Login from './container/login/login.js';
-import AuthRoute from  './component/authroute/authroute'
-import * as serviceWorker from './serviceWorker';
+// import AuthRoute from  './component/authroute/authroute'
 
-const store = createStore(reducers, compose(
-    applyMiddleware(thunk),
+
+//const store = createStore(reducers, compose(
+  //  applyMiddleware(thunk),
     //redux调试
-    window.devToolsExtension ? window.devToolsExtension() : f => f
+ //   window.devToolsExtension ? window.devToolsExtension() : f => f
 
-))
+//))
 
 ReactDOM.render(
 
-    <Provider store={store} >
+    
         <BrowserRouter>
 
             <div>
-                <AuthRoute></AuthRoute>
+            
+                <Route path='/login' component={Login}></Route>
                 
             </div>
 
         </BrowserRouter>
 
 
-
-
-    </Provider>, document.getElementById('root'));
+   , document.getElementById('root'));
---
+++ src/redux/user.redux.js
@@ -0,0 +1,42 @@
+import axios from 'axios'
+
+const ERROR_MSG = 'ERROR_MSG'
+const AUTH_SUCCESS = 'AUTH_SUCCESS'
+const initState = {
+    user: '',
+    pwd: '',
+    rem: true
+}
+export function user(state = initState, action) {
+    switch (action.type) {
+        case AUTH_SUCCESS:
+            return { ...state, msg: '', ...action.payload }
+        case ERROR_MSG:
+            return { ...state, msg: action.msg }
+        default:
+            return state
+    }
+}
+
+
+
+function authSuccess(obj) {
+    const { pwd, ...data } = obj
+    return { type: AUTH_SUCCESS, payload: data }
+}
+function errorMsg(msg) {
+    return { msg, type: ERROR_MSG }
+}
+
+export function login({ user, pwd, rem }) {
+    return dispatch => {
+        axios.post('/user/update')
+            .then(res => {
+                if (res.status === 200 & res.data.code === 0) {
+                    dispatch(authSuccess(res.data.data))
+                } else {
+                    dispatch(errorMsg(res.data.msg))
+                }
+            })
+    }
+}
