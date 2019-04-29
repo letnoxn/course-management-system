069bb439584593ef5306c925e9f0f42ae0f1263e
wangsen <wangsen@caicloud.io>
2019-04-29 15:42:18 +0800

    fix(no-data): 当没有评论时隐藏评论组件

--- src/component/chat/chat.js
+++ src/component/chat/chat.js
@@ -84,18 +84,20 @@ class Chat extends React.Component {
                                 />
                                 {item.content}
                             </List.Item>
-                            <List
-                                renderItem={item => (
-                                    <List.Item>
-                                        <List.Item.Meta
-                                            avatar={<Avatar src="https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png" />}
-                                            title={<a href="https://ant.design">{item.title}</a>}
-                                            description="Ant Design, a design language for background applications, is refined by Ant UED Team"
-                                        />
-                                    </List.Item>
+                            {item.comments.length !== 0 && (
+                                <List
+                                    renderItem={item => (
+                                        <List.Item>
+                                            <List.Item.Meta
+                                                avatar={<Avatar src="https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png" />}
+                                                title={<a href="https://ant.design">{item.title}</a>}
+                                                description="Ant Design, a design language for background applications, is refined by Ant UED Team"
+                                            />
+                                        </List.Item>
 
-                                )}
-                            />
+                                    )}
+                                />
+                            )}
 
 
 
